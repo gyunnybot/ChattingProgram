751815d72b8bc1330b71d9ed27a22519a4ec6cf0
gyunnybot <idjkda4034@gmail.com>
2025-06-14 07:58:38 +0900

    Update ServerMain.java

--- chat/server/ServerMain.java
+++ chat/server/ServerMain.java
@@ -7,12 +7,12 @@ public class ServerMain {
     public static void main(String[] args) throws IOException {
         SessionManager sessionManager = new SessionManager();
 
-        //CommandManager commandManagerV1 = new CommandManagerV1(sessionManager);
-        //CommandManager commandManagerV2 = new CommandManagerV2(sessionManager);
-        //CommandManager commandManagerV3 = new CommandManagerV3(sessionManager);
-        CommandManager commandManagerV4 = new CommandManagerV4(sessionManager);
-
-        Server server = new Server(PORT, commandManagerV1, sessionManager);
+        //CommandManager commandManager = new CommandManagerV1(sessionManager);
+        //CommandManager commandManager = new CommandManagerV2(sessionManager);
+        //CommandManager commandManager = new CommandManagerV3(sessionManager);
+        CommandManager commandManager = new CommandManagerV4(sessionManager);
+        
+        Server server = new Server(PORT, commandManager, sessionManager);
         server.start();
     }
 }
